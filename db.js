60032b7d0deec5a4f88771261432a956ab9e77ef
theunkown259 <jaclynrae63@gmail.com>
2023-11-27 16:40:47 -0700

    History working with the correct schema

--- db.js
+++ db.js
@@ -10,4 +10,24 @@ const db = async () => {
     }
 }
 
-module.exports = {db}
+///
+
+const transactionSchema = new mongoose.Schema({
+    TransactionId: { type: Number, required: true },
+    UserId: { type: Number, required: true },
+    CategoryId: { type: Number, required: true },
+    Type: { type: String, required: true },
+    Amount: { type: Number, required: true },
+    Currency: { type: String, required: true },
+    Date: { type: String, required: true },
+    Description: { type: String, required: true },
+});
+
+const Yourmodel = new mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    Transactions: [transactionSchema],
+});
+const Testingmodel = mongoose.model('mytest', Yourmodel);
+///
+
+module.exports = {db,Testingmodel}
--- index.js
+++ index.js
@@ -2,33 +2,19 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
-const {db} = require('./db.js');
+const { db, Testingmodel } = require('./db.js');
 
 const app = express();
-app.set('views','./views');
-app.set('view engine','ejs');
+app.set('views', './views');
+app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 app.use(express.static('./public/css/'));
 app.use(express.static(__dirname + '/public'));
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
-///
-const testingSchema = new mongoose.Schema({
-    TransactionId: Number,
-    UserId: Number,
-    CategoryId: Number,
-    Type: String,
-    Amount: Number,
-    Currency: Number,
-    date: { type: Date, default: Date.now },
-    Description: String,
-});
-
-const Testingmodel = mongoose.model('testings',testingSchema);
 // Sample data (replace this with your actual budget data)
 const budgetCategories = [
     { name: 'Groceries', budget: 500, spent: 300 },
@@ -45,24 +31,35 @@ const categoriesWithRemainingBudget = budgetCategories.map(category => {
     return { ...category, remainingBudget };
 });
 const sortedCategories = categoriesWithRemainingBudget.sort((a, b) => a.remainingBudget - b.remainingBudget);
+
 // Start the server
-app.get('/', async(req,res) => {
+app.get('/', async (req, res) => {
     const usernum = 2;
-    try{
+    try {
         await db();
         const count = parseInt(req.query.count, 10) || 3;
 
         // Slice the array to get the specified number of categories
         const slicedCategories = sortedCategories.slice(0, count);
 
-        const transactions = await Testingmodel.find({UserId:usernum}).lean().exec();
+        const transactions = await Testingmodel.find({ UserId: usernum }).lean().exec();
         //recent History
 
-        const last5Transactions = await Testingmodel.find({UserId:usernum})
-        .sort({ date: -1 })
-        .limit(5)
-        .lean()
-        .exec();
+        const last5Transactions = await Testingmodel.aggregate([
+            { $match: { 'Transactions.UserId': usernum } },
+            { $unwind: '$Transactions' },
+            { $sort: { 'Transactions.Date': -1 } },
+            { $limit: 5 },
+            {
+              $project: {
+                'Transactions.Date': 1,
+                'Transactions.Amount': 1,
+                'Transactions.Type': 1,
+              },
+            },
+          ]).exec();
+          
+console.log(last5Transactions)
 
         const totalIncome = transactions
             .filter(transaction => transaction.Type === 'Income')
@@ -75,27 +72,36 @@ app.get('/', async(req,res) => {
         var totalBalance = totalIncome - totalExpenses;
 
         const expenses = transactions
-            .filter(transaction => transaction.Type === 'Expense')
+            .filter(transaction => transaction.Type === 'Expense');
 
         // Calculate categoryLabels and categoryAmounts
         const categoryLabels = Array.from(new Set(expenses.map(expense => expense.CategoryId)));
+        const categoryDescription = Array.from(new Set(expenses.map(expense => expense.Description)));
         const categoryAmounts = categoryLabels.map(categoryId =>
             expenses.filter(expense => expense.CategoryId === categoryId)
-            .reduce((total, expense) => total + expense.Amount, 0)
+                .reduce((total, expense) => total + expense.Amount, 0)
         );
 
+        res.render('index.ejs', {
+           last5Transactions,
+            totalBalance,
+            transactions,
+            totalExpenses,
+            totalIncome,
+            categoriesWithRemainingBudget: slicedCategories,
+            count,
+            categoryLabels,
+            categoryAmounts,
+            categoryDescription
+        });
 
-
-    res.render('index.ejs', {newesttransactions:last5Transactions, totalBalance,transactions,totalExpenses,totalIncome, categoriesWithRemainingBudget:slicedCategories,count,categoryLabels, categoryAmounts });
-
-    }catch(error){
+    } catch (error) {
         res.status(500).send("Oh no! There's a Server Error.");
-        console.log("Internal Server Error");
+        console.log("Internal Server Error", error);
     }
-
 });
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
-  console.log(`WebSocket is open on http://localhost:${port}`);
+    console.log(`WebSocket is open on http://localhost:${port}`);
 });
